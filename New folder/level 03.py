dc3216f9b41ed1b6d05893a3feefae72bf1915ed
davidus-gh <david.gulua@icloud.com>
2025-07-27 18:52:42 +0400

    first commit

---
+++ New folder/level 03.py
@@ -0,0 +1,48 @@
+num1 = 9 
+num2 = 10
+num3 = 79
+
+print(num1 + num2 + num3)
+
+x = 987
+y = 999
+
+print(x + y)
+
+
+width = 10
+length = 20
+#P = 20x2 + 10x2 = 40 + 20 = 60
+print(2 * width + 2 * length)
+
+#mimateba
+
+print(9 + 10)
+print(89 + 9873)
+print("5" + "67")
+print(8 + 9)
+print("99" + "00")
+
+#gamokleba
+
+print(100 - 90)
+print(900 - 1000)
+print(99 - 99)
+print(99 - 100)
+print(99 - 0)
+
+#gamravleba
+
+print(3 * 90)
+print(3 * "6")
+print(9 * "0")
+print(6 * 3)
+print(98876 * 7742)
+
+#gayopa
+
+print(9/3)
+print(10/1)
+print(99.9//11.1)
+print(9//9)
+print(984723//33)
