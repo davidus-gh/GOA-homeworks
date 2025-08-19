0ec356ea24cc2a174bc063cf17b09739054f046e
davidus-gh <david.gulua@icloud.com>
2025-08-19 11:03:23 +0400

    Add files via upload

---
+++ level07.py
@@ -0,0 +1,64 @@
+#input - სჰეგვაქ ინფრომაცია კომპიუტერში
+#output  - კომპიუტერი გამოსახავს სჰეტანილ ინფოს
+
+#konkatinacia - ori stringis sheerteba
+
+
+print(float(input("height:")))
+
+
+
+
+a = "8"
+
+b = int(a)
+
+c = 9
+
+print("a" + "b")
+
+
+
+# pythons  აქვს რამდენიმე data type,data covnesation  გვხემარება ვიხმაროდ სხვადასხვა data,იმის შესაბამისად,თუ რომელი უფრო გვაწყობს და გვიადვილებს საქმეს
+
+#implicit - თვითონ პროგრამას გადაყავს ერთი data tyoe მეორე არჩეულ data typrში
+
+
+
+
+
+#explicit - მომხმარებლის დახმარებით გადაყავს ერთი data tyoe მეორე არჩეულ data typrში
+# მაგ :
+# float():
+# int():
+# str():
+
+
+
+#float
+a = 5
+
+n = float(a)
+
+print(n)
+print(type(n))
+
+#integer
+
+a = 5.9
+
+n = int(a)
+
+print(n)
+print(type(n))
+
+
+#string
+
+a = 5
+
+n = str(a)
+
+print(n)
+print(type(n))
+
